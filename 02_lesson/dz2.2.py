3f91808c9c2196628320fe8ead25f861de612957
Sergey Pichakhchi <s.pichakhchi@gmail.com>
2024-04-19 11:41:04 +0300

    dz2

--- 02_lesson/dz2.2.py
+++ 02_lesson/dz2.2.py
@@ -1,2 +1,3 @@
 a = int(input("Please enter five digit number:"))
-print(a%10,  a//10%10, a//100%10, a//1000%10, a//10000, sep="")
+b = ((a%10*10000) + (a//10%10*1000) + (a//100%10*100) + (a//1000%10*10) + a//10000)
+print(b)
