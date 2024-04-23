4b76efe81ce0a0daa8c5b3147f292c1895579feb
Sergey Pichakhchi <s.pichakhchi@gmail.com>
2024-04-23 12:38:21 +0300

    added dz3.3_v2

--- 03_lesson/dz3.3_v2.py
+++ 03_lesson/dz3.3_v2.py
@@ -1,10 +1,12 @@
 lst = [1, 2, 3]
 lst2 = []
-l1 = len(lst) // 2 + 1
-if len(lst) % 2 == 0 and len(lst) != 0:
-    lst2.append(lst[:len(lst) // 2])
-    lst2.append(lst[len(lst) // 2:])
+
+if len(lst) % 2 == 0:
+    l1 = len(lst) //2
+    lst2.append(lst[:l1])
+    lst2.append(lst[l1:])
 else:
+    l1 = len(lst) // 2 +1
     lst2.append(lst[:l1])
     lst2.append(lst[l1:])
 
