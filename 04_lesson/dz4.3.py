9e884703b047a312b2ff7f5f23348e612c688f36
Sergey Pichakhchi <s.pichakhchi@gmail.com>
2024-04-27 19:52:38 +0300

    dz4.2

---
+++ 04_lesson/dz4.3.py
@@ -0,0 +1,14 @@
+import random
+list1 = []
+list2 = []
+for i in range(random.randint(3,10)):
+    list1.append(random.randint(0,10))
+else:
+    print(list1,"=> ", end="")
+
+for j in 0,2,-2:
+    list2.append(list1[j])
+else:
+    print(list2)
+
+
