47b497aa97c90e848bf4d72c3eea14f60b23faf8
Sergey Pichakhchi <s.pichakhchi@gmail.com>
2024-05-01 14:19:44 +0300

    Fix DZ5.1 optimization

--- 05_lesson/dz5.1_check_value.py
+++ 05_lesson/dz5.1_check_value.py
@@ -2,28 +2,23 @@ import keyword
 import string
 
 string1 = input()
-string_result = "TRUE"
 if string1.startswith("__"):
     exit("False")
 if string1[0].isdigit() or ' ' in string1:
-    string_result = "FALSE"
     exit("False")
 
 for i in string1:
     if i.isupper():
-        string_result = "FALSE"
         exit("False")
 
 for j in string.punctuation:
     if j == "_":
         continue
     elif j in string1:
-        string_result = "FALSE"
         exit("False")
 
 for f in keyword.kwlist:
     if f in string1 and "_" not in string1:
-        string_result = "FALSE"
         exit("False")
 
-print("It is: ", string_result)
+print("True")
