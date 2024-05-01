0ef94429f56d8f3c9431b8a5bc48f021cced33f4
Sergey Pichakhchi <s.pichakhchi@gmail.com>
2024-05-01 14:39:00 +0300

    dz5.3 fix

--- 05_lesson/dz5.3_hashtag.py
+++ 05_lesson/dz5.3_hashtag.py
@@ -3,8 +3,9 @@ import string
 string1 = input("Enter string: ")
 string3 = ""
 
-for i in (string1.title().replace(" ", "")):
+for i in string1:
     if i not in string.punctuation:
         string3 += i
 
-print("#" + string3[:138])
+
+print("#" + string3[:139].title().replace(" ", ""))
