81dc4c367996abee7f1705cf25b8e9a90614b16d
Sergey Pichakhchi <s.pichakhchi@gmail.com>
2024-05-04 12:25:40 +0300

    Added dz6.2 v2

---
+++ 06_lesson/dz6.2_convert_v2.py
@@ -0,0 +1,14 @@
+time1 = int(input())
+if 0 <= time1 <= 8640000:
+    day_count = time1 // (24 * 60 * 60)
+    hours_count = time1 % (24 * 60 * 60) // (60 * 60)
+    minutes_count = time1 % (60 * 60) // 60
+    seconds_count = time1 % 60
+
+    print("{} days, {}:{}:{}".format(str(day_count), str(hours_count).zfill(2),str(minutes_count).zfill(2),str(seconds_count).zfill(2)))
+
+
+else:
+    exit("error number")
+
+
