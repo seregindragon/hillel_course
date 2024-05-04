5d92b6571860e2d31acc738f0d8a6eb3e936b04e
Sergey Pichakhchi <s.pichakhchi@gmail.com>
2024-05-04 18:12:05 +0300

    Added dz6.2 v2 fix

--- 06_lesson/dz6.2_convert_v2.py
+++ 06_lesson/dz6.2_convert_v2.py
@@ -5,8 +5,16 @@ if 0 <= time1 <= 8640000:
     minutes_count = time1 % (60 * 60) // 60
     seconds_count = time1 % 60
 
-    print("{} days, {}:{}:{}".format(str(day_count), str(hours_count).zfill(2), str(minutes_count).zfill(2),
+    if day_count % 10 == 1 and day_count != 11 :
+        print("{} день, {}:{}:{}".format(str(day_count), str(hours_count).zfill(2), str(minutes_count).zfill(2),
                                      str(seconds_count).zfill(2)))
+    elif (day_count % 10 == 1 or day_count %10 == 3 or day_count %10 == 4 or day_count %10 == 2) and day_count != 11:
+        print("{} дні, {}:{}:{}".format(str(day_count), str(hours_count).zfill(2), str(minutes_count).zfill(2),
+                                     str(seconds_count).zfill(2)))
+    else:
+        print("{} днів, {}:{}:{}".format(str(day_count), str(hours_count).zfill(2), str(minutes_count).zfill(2),
+                                    str(seconds_count).zfill(2)))
+
 
 
 else:
