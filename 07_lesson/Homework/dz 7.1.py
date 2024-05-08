876c4a4b6f41f6bfd0b85211e04fe0ad5f97a1ff
Sergey Pichakhchi <s.pichakhchi@gmail.com>
2024-05-08 12:54:38 +0300

    done hometask: 7.1 fixed

--- 07_lesson/Homework/dz 7.1.py
+++ 07_lesson/Homework/dz 7.1.py
@@ -3,6 +3,8 @@ def say_hi(name: str, age: int) -> str:
     return str1
 
 
-assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old"
-assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old"
-print('ОК')
+if __name__ == "__main__":
+    assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old"
+    assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old"
+    assert say_hi("Sergey", 35) == "Hi. My name is Sergey and I'm 35 years old"
+    print('ОК')
