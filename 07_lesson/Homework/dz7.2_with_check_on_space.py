608dba8e78dfa1d11d76e96c741f3eda5ed6967e
Sergey Pichakhchi <s.pichakhchi@gmail.com>
2024-05-08 12:52:41 +0300

    done hometask: 7.2

---
+++ 07_lesson/Homework/dz7.2_with_check_on_space.py
@@ -0,0 +1,25 @@
+def correct_sentence(text: str) -> str:
+    if text[-1] != " ":
+        if text[0].upper() != text[0] and text[-1] != ".":
+            str1 = text[0].upper() + text[1:] + "."
+        elif text[0].upper() == text[0] and text[-1] != ".":
+            str1 = text + "."
+        elif text[0].upper() != text[0] and text[-1] == ".":
+            str1 = text[0].upper() + text[1:]
+        else:
+            str1 = text
+    else:
+        str1 = correct_sentence(text[:-1])
+
+    return str1
+
+
+if __name__ == "__main__":
+    assert correct_sentence("greetings, friends") == "Greetings, friends."
+    assert correct_sentence("hello") == "Hello."
+    assert correct_sentence("Greetings. Friends") == "Greetings. Friends."
+    assert correct_sentence("Greetings, friends.") == "Greetings, friends."
+    assert correct_sentence("greetings, friends.") == "Greetings, friends."
+    assert correct_sentence("hello, friend   ") == "Hello, friend."
+    assert correct_sentence("hello my dead friend,need meet you") == "Hello my dead friend,need meet you."
+    print('ОК')
