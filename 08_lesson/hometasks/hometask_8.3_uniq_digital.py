31630c4bffbbd84ce69376f98986cd0e24ace5dc
Sergey Pichakhchi <s.pichakhchi@gmail.com>
2024-05-11 18:49:28 +0300

    hometask_8.3

--- 08_lesson/hometasks/hometask_8.3_uniq_digital.py
+++ 08_lesson/hometasks/hometask_8.3_uniq_digital.py
@@ -4,7 +4,8 @@ def find_unique_value(some_list: list) -> int | float:
             return i
 
 
-assert find_unique_value([1, 2, 1, 1]) == 2
-assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2
-assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5
-print("ОК")
+if __name__ == "__main__":
+    assert find_unique_value([1, 2, 1, 1]) == 2
+    assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2
+    assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5
+    print("ОК")
