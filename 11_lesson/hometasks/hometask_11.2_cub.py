fe1f0177c1ab5a2d66db78fee45f1b0af585b908
Sergey Pichakhchi <s.pichakhchi@gmail.com>
2024-05-23 10:56:03 +0300

    Hometask 11.2

---
+++ 11_lesson/hometasks/hometask_11.2_cub.py
@@ -0,0 +1,20 @@
+from inspect import isgenerator
+
+
+def generate_cube_numbers(end: int):
+    for i in range(2,end+1):
+        if i**3 > end:
+            return
+        else:
+            yield i**3
+
+
+
+
+
+gen = generate_cube_numbers(1)
+assert isgenerator(gen) is True
+assert list(generate_cube_numbers(10)) == [8]
+assert list(generate_cube_numbers(100)) == [8, 27, 64]
+assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729, 1000]
+print("OK")
